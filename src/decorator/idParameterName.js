3f20709dfc210d3e943edd448ece274d100cc58d
Martin Jurča <mjurca@centrum.cz>
2016-04-12 22:10:25 +0200

    documentation

--- src/decorator/idParameterName.js
+++ src/decorator/idParameterName.js
@@ -1,4 +1,18 @@
 
+/**
+ * Decorator for setting the static {@code idParameterName} property of an
+ * entity class, which specifies the name of the ID parameter of an entity in
+ * the entity's resource's links.
+ *
+ * @param {string} idParameterName The name of the link parameters used to
+ *        specify the ID(s) of the entity(ies).
+ * @return {function(function(
+ *             new: AbstractHalsonEntity,
+ *             restClient: HalsonRestClient,
+ *             data: Object<string, *>
+ *         ))} Callback that sets the ID link parameter name on the class
+ *         provided to it.
+ */
 export default (idParameterName) => {
 	return (classConstructor) => {
 		Object.defineProperty(classConstructor, 'idParameterName', {
