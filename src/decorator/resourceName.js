262cb70c3578a8de950cfa42032cc7cfd9f83e5e
Martin Jurča <mjurca@centrum.cz>
2016-04-22 17:34:19 +0200

    finished decorators documentation

--- src/decorator/resourceName.js
+++ src/decorator/resourceName.js
@@ -1,4 +1,18 @@
 
+/**
+ * Decorator for setting the static {@code resourceName} property of an entity
+ * class, which specifies the name of the REST API resource to use with the
+ * provided entity class.
+ *
+ * @param {string} resourceName The name of the REST API resource, as
+ *        identified in the REST API's resource links map.
+ * @return {function(function(
+ *             new: AbstractHalsonEntity,
+ *             restClient: HalsonRestClient,
+ *             data: Object<string, *>
+ *         ))} Callback that sets the resource name on the class provided to
+ *         it.
+ */
 export default (resourceName) => {
 	return (classConstructor) => {
 		Object.defineProperty(classConstructor, 'resourceName', {
