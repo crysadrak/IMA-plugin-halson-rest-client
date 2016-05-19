cfedde7fd290d3c2b661cd76a7aa9c4b7c829f7f
Martin Jurča <martin.jurca@firma.seznam.cz>
2016-05-19 15:46:37 +0200

    updated the base entity class to be compatible with the new base rest api client, added documentation

--- src/AbstractHalsonEntity.js
+++ src/AbstractHalsonEntity.js
@@ -1,91 +1,57 @@
 
-export default class AbstractHalsonEntity {
-	constructor(restClient, data) {
-		this._links = {};
+import AbstractEntity from 'ima-plugin-rest-client/src/AbstractEntity';
+
+/**
+ * The base class for typed REST API HALSON entities. The resources in the REST
+ * API will be identified by classes extending this one by the HALSON REST API
+ * client.
+ */
+export default class AbstractHalsonEntity extends AbstractEntity {
+	/**
+	 * 
+	 * @param {HalsonRestClient} restClient REST API client.
+	 * @param {Object<string, *>} data Entity data, which will be directly
+	 *        assigned to the entity's fields.
+	 * @param {?AbstractHalsonEntity=} parentEntity The entity within which the
+	 *        resource containing this entity is located. Can be set to
+	 *        {@code null} if this entity belongs to a top-level resource
+	 *        without a parent.
+	 */
+	constructor(restClient, data, parentEntity = null) {
+		super(restClient, data, parentEntity);
+		
+		if (!this._links) {
+			this._links = {};
+		}
 		Object.defineProperty(this, '_links', {
 			enumerable: false
 		});
-		
-		Object.assign(this, data);
-		
-		this._restClient = restClient;
-		Object.defineProperty(this, '_restClient', {
-			enumerable: false
-		});
 	}
-	
-	static get resourceName() {
-		throw new Error('The resourceName getter is abstract and must be ' +
+
+	/**
+	 * Returns the name of the ID parameter used in this entity's resource to
+	 * identify individual entities.
+	 * 
+	 * @return {string} The name of the ID parameter used in this entity's
+	 *         resource to identify individual entities.
+	 */
+	static get idParameterName() {
+		throw new Error('The idParameterName getter is abstract and must be ' +
 				'overridden');
 	}
 
-	static get idParameterName() {
-		return 'id';
-	}
-
-	static get idFieldName() {
-		return 'id';
-	}
-	
+	/**
+	 * Returns the names of the embedded resources that should be inlined into
+	 * the entity's fields.
+	 * 
+	 * The embed names may contain prefixes separated by a colon ({@code :}) from
+	 * the resource name. The prefixes will not be included in the names of the
+	 * entity fields into which the embedded resources will be inlined.
+	 * 
+	 * @return {?string[]} The names of the embedded resources that should be
+	 *         inlined into the entity's fields.
+	 */
 	static get inlineEmbeds() {
 		throw null;
-	}
-	
-	static get inlineResponseBody() {
-		return false;
-	}
-
-	get parentEntity() {
-		return null;
-	}
-	
-	static list(restClient, parameters = {}, options = {}) {
-		return restClient.list(this, parameters, options);
-	}
-	
-	static get(restClient, id, parameters = {}, options = {}) {
-		return restClient.get(this, id, parameters, options);
-	}
-
-	static create(restClient, data, options = {}) {
-		let instance = new this(restClient, data);
-		return instance.create(options);
-	}
-
-	static delete(restClient, id, options = {}) {
-		return restClient.delete(this, id, options);
-	}
-
-	list(subResource, parameters = {}, options = {}) {
-		return this._restClient.list(subResource, parameters, options, this);
-	}
-
-	get(subResource, id, parameters = {}, options = {}) {
-		let client = this._restClient;
-		return client.get(subResource, id, parameters, options, this);
-	}
-
-	patch(data, options = {}) {
-		let resource = this.constructor;
-		let id = this[this.constructor.idFieldName];
-		let client = this._restClient;
-		return client.patch(resource, id, data, options).then((response) => {
-			Object.assign(this, data);
-			return response;
-		});
-	}
-
-	replace(options = {}) {
-		let id = this[this.constructor.idFieldName];
-		return this._restClient.replace(this.constructor, id, this, options);
-	}
-
-	create(options = {}) {
-		return this._restClient.create(this.constructor, this, options);
-	}
-
-	delete(options = {}) {
-		let id = this[this.constructor.idFieldName];
-		return this.constructor.delete(this._restClient, id, options);
 	}
 }
