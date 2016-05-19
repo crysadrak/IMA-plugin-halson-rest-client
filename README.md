f5513730bb5e2dcab318e021acc2118a4138ed17
Martin Jurča <martin.jurca@firma.seznam.cz>
2016-05-19 16:01:23 +0200

    the link generator now utilizes the uri-template npm package to process the URI templates

--- README.md
+++ README.md
@@ -6,10 +6,3 @@ HAL specification:
 
 http://stateless.co/hal_specification.html or
 https://tools.ietf.org/html/draft-kelly-json-hal-07 (already expired).
-
-This plugin does not fully implement the URI template RTC
-(https://tools.ietf.org/html/rfc6570), only the `{var}` and `{var1,var2}`
-notations are supported.
-
-A full support of URI templates may be added in the future using the
-`uri-template` npm module.
--- src/HalsonLinkGenerator.js
+++ src/HalsonLinkGenerator.js
@@ -1,23 +1,34 @@
 
 import LinkGenerator from 'ima-plugin-rest-client/src/LinkGenerator';
+import uriTemplateParser from 'uri-template';
 
 /**
- * The separator of query string's key-value pairs. While the W3C
- * recommendation
- * https://www.w3.org/TR/1999/REC-html401-19991224/appendix/notes.html#h-B.2.2
- * allows both ampersands {@code &} and semicolons {@code ;} to be used in
- * {@code application/x-www-form-urlencoded}-encoded query strings, some
- * servers support only the ampersands, so we'll use the ampersand to ensure
- * better compatibility with various server implementations.
+ * Private field symbols.
  *
- * @type {string}
+ * @type {Object<string, symbol>}
  */
-const QUERY_PARAMETER_SEPARATOR = '&';
+const PRIVATE = Object.freeze({
+	uriTemplates: Symbol('uriTemplates')
+});
 
 /**
  * URI generator for the HAL+JSON REST API client.
  */
 export default class HalsonLinkGenerator extends LinkGenerator {
+	/**
+	 * Initializes the link generator.
+	 */
+	constructor() {
+		/**
+		 * Cache of parsed URI templates. The keys are the raw URI templates.
+		 *
+		 * @type {Map<string, {expand: function(Object<string, (number|string|(number|string)[])>): string}>}
+		 */
+		this[PRIVATE.uriTemplates] = new Map();
+
+		Object.freeze(this);
+	}
+
 	/**
 	 * @inheritdoc
 	 * @override
@@ -58,12 +69,6 @@ export default class HalsonLinkGenerator extends LinkGenerator {
 	 * Processes the provided URI template by replacing the placeholders with
 	 * the provided parameter values.
 	 *
-	 * The method does not fully implement the RFC 6570 (URI Templates), only
-	 * the {@code {var}} and {@code {var1,var2}} notations are supported.
-	 *
-	 * Any parameters that were not used to replace a URI template placeholder
-	 * will be added as query parameters to the end of the generated URI.
-	 *
 	 * @private
 	 * @param {string} template URI template to process.
 	 * @param {Object<string, (number|string|(number|string)[])>} parameters
@@ -72,33 +77,15 @@ export default class HalsonLinkGenerator extends LinkGenerator {
 	 * @return {string} Generated URI.
 	 */
 	_processURITemplate(template, parameters, apiRoot) {
-		let link = template;
-		let unusedParameters = Object.assign({}, parameters);
-		
-		while (link.indexOf('{') > -1) {
-			let placeholderStart = link.indexOf('{');
-			let placeholderEnd = link.indexOf('}', placeholderStart);
-			if (placeholderEnd === -1) {
-				throw new Error(`Invalid link template: ${template}`);
-			}
-			
-			let placeholder = link.slice(placeholderStart, placeholderEnd + 1);
-			let parameterNames = placeholder.slice(1, -1).split(',');
-			let values = parameterNames.map((parameterName) => {
-				delete unusedParameters[parameterName];
-				return parameters[parameterName];
-			});
-			
-			link = link.replace(placeholder, values);
+		let parsedTemplate;
+		if (this[PRIVATE.uriTemplates].has(template)) {
+			parsedTemplate = this[PRIVATE.uriTemplates].get(template);
+		} else {
+			parsedTemplate = uriTemplateParser.parse(template);
+			this[PRIVATE.uriTemplates].set(template, parsedTemplate);
 		}
-		
-		if (Object.keys(unusedParameters).length) {
-			link += (link.indexOf('?') > -1 ? QUERY_PARAMETER_SEPARATOR : '?');
-			link += LinkGenerator.encodeQuery(
-				unusedParameters,
-				QUERY_PARAMETER_SEPARATOR
-			);
-		}
+
+		let link = parsedTemplate.expand(parameters);
 
 		if (link.substring(0, 1) === '/') {
 			link = `${apiRoot}${link}`;
